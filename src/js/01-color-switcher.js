36dcff3a5c8cc2ec7de8a05e31f5654f70b16d11
MorskoySom <Somovvitvit@gmail.com>
2023-08-16 14:24:04 +0300

    2

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const elem = {
+    btnStart: document.querySelector('[data-start]'),
+    btnStop: document.querySelector('[data-stop]'),
+    body: document.querySelector('body')
+}
+console.log(elem.btnStart);
+console.log(elem.btnStop);
+console.log(elem.body);
 
+elem.btnStart.addEventListener(`click`, handlerStart);
+elem.btnStop.addEventListener(`click`, handlerStop);
+
+let id;
+function handlerStart() {
+    elem.btnStart.disabled = "disabled";
+    id = setInterval(() => {
+        const rundomColor = getRandomHexColor();
+        elem.body.style.backgroundColor = rundomColor;
+    }, 1000)
+}
+
+function handlerStop() {
+    elem.btnStart.disabled = false;
+    clearInterval(id);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
