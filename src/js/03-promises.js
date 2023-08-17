7553b4364f06473301984c5203f6a60b48a1af10
MorskoySom <Somovvitvit@gmail.com>
2023-08-17 18:22:37 +0300

    310

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,4 @@
 import Notiflix from "notiflix";
-
 const form = document.querySelector('.form');
 const elem = {
   delay: form.querySelector('input[name="delay"]'),
@@ -7,47 +6,102 @@ const elem = {
   amount: form.querySelector('input[name="amount"]'),
   btn: form.querySelector('button')
 }
-
-elem.btn.addEventListener('click', handlerClick);
-
-function handlerClick() {
+form.addEventListener('submit', handlerSubmit);
+function handlerSubmit(event) {
+  event.preventDefault(); // +
   const inAmount = Number(elem.amount.value);
   const inputDelay = Number(elem.delay.value);
   const inputStep = Number(elem.step.value);
-  // generatePromises(inAmount, inputDelay, inputStep)
-  console.log(createPromise(0, inputDelay))
+  generatePromises(inAmount, inputDelay, inputStep);
 }
-
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(`Promise ${position} resolved after ${delay} ms`);
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
-        reject(`Promise ${position} rejected after ${delay} ms`);
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
       }
     }, delay);
   });
 }
-
 function generatePromises(inAmount, inputDelay, inputStep) {
   const promises = [];
   for (let i = 0; i < inAmount; i++) {
     const currentDelay = inputDelay + i * inputStep;
     const position = i + 1;
-    const promise = new Promise(resolve => {
-      setTimeout(() => {
-        resolve(createPromise(position, currentDelay));
-      }, currentDelay);
-    });
+    const promise = createPromise(position, currentDelay);
     promises.push(promise);
   }
+  Promise.all(promises)
+    .then(results => {
+      for (const result of results) {
+        console.log(result);
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
+
+
+
+
+
+
+// import Notiflix from "notiflix";
+
+// const form = document.querySelector('.form');
+// const elem = {
+//   delay: form.querySelector('input[name="delay"]'),
+//   step: form.querySelector('input[name="step"]'),
+//   amount: form.querySelector('input[name="amount"]'),
+//   btn: form.querySelector('button')
+// }
+
+// elem.btn.addEventListener('click', handlerClick);
+
+// function handlerClick() {
+//   const inAmount = Number(elem.amount.value);
+//   const inputDelay = Number(elem.delay.value);
+//   const inputStep = Number(elem.step.value);
+//   // generatePromises(inAmount, inputDelay, inputStep)
+//   console.log(createPromise(0, inputDelay))
+// }
+
+// function createPromise(position, delay) {
+//   return new Promise((resolve, reject) => {
+//     const shouldResolve = Math.random() > 0.3;
+//     setTimeout(() => {
+//       if (shouldResolve) {
+//         resolve(`Promise ${position} resolved after ${delay} ms`);
+//         Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//         console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//       } else {
+//         reject(`Promise ${position} rejected after ${delay} ms`);
+//         Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+//         console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//       }
+//     }, delay);
+//   });
+// }
+
+// function generatePromises(inAmount, inputDelay, inputStep) {
+//   const promises = [];
+//   for (let i = 0; i < inAmount; i++) {
+//     const currentDelay = inputDelay + i * inputStep;
+//     const position = i + 1;
+//     const promise = new Promise(resolve => {
+//       setTimeout(() => {
+//         resolve(createPromise(position, currentDelay));
+//       }, currentDelay);
+//     });
+//     promises.push(promise);
+//   }
+// }
 
 
 
